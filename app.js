4fba428f53b0a424efed0505b0aced32370f99a8
salvinanto7 <salvinanto7@gmail.com>
2022-07-26 14:50:52 +0530

    user pages added

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
-
+var hbs = require('express-handlebars');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user/index',{admin:false});
 });
+
+router.get('/train',(req,res)=>{
+  res.render('user/train',{admin:false})
+});
+
+router.get('/ticket',(req,res)=>{
+  res.render('user/ticket',{admin:false})
+});
+
+
 
 module.exports = router;
